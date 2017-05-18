ca2f1f2a52788b125b838e8cd15833642c4134e7
Yannic <zebybez@gmail.com>
2017-05-18 23:10:35 +0200

    empty util class, and thoughts about the implementation

--- core/src/main/java/Crawler.java
+++ core/src/main/java/Crawler.java
@@ -1,4 +1,9 @@
 import model.WebsiteEntity;
+import util.HTMLParser;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by Marc on 10-5-2017.
@@ -9,16 +14,19 @@ public class Crawler {
     private String startUrl;
     private int maxDepth;
 
+    private HTMLParser parser;
+
     public Crawler(String startUrl, int maxDepth) {
         this.startUrl = startUrl;
         this.maxDepth = maxDepth;
+        parser = new HTMLParser();
     }
 
     public String getStartUrl() {
         return startUrl;
     }
 
-    public void setStartUrl(String startUrl) {
+    private void setStartUrl(String startUrl) {
         this.startUrl = startUrl;
     }
 
@@ -30,7 +38,41 @@ public class Crawler {
         this.maxDepth = maxDepth;
     }
 
+    private WebsiteEntity parseUrl(String siteUrl) throws Exception{
+        WebsiteEntity w = new WebsiteEntity();
+        w.setUrl(siteUrl);
+        parser.setUrl(siteUrl);
+
+        //todo bedenk hoe pages worden aangemaakt, en hoe ze worden gebruikt.
+        //setPageLinks kan de pages zelf aanmaken, is er dan ook een homepage page in website?
+        //hoe worden pages in pages aan de website toegevoegd?
+        //
+        w.setPageLinks(parser.getPageLinks());
+        w.setExtLinks(parser.getExtLinks());
+
+        return null;
+    }
+
+    /**
+     * Creates the first website entity with the entered parameters
+     * @return a WebsiteEntity or null if errors occur
+     */
     public WebsiteEntity start() {
+        try {
+            return parseUrl(startUrl);
+
+        } catch (Exception e){
+            Logger.getLogger(this.getClass().toString()).log(Level.WARNING, "Error while parsing URL");
+        }
+        return null;
+    }
+
+    /**
+     * loops through the list of external links in the given website and parses their HTML;
+     * @param website
+     * @return A list of Websites.
+     */
+    public List<WebsiteEntity> getExternalSitesFromSite(WebsiteEntity website){
         return null;
     }
 }
---
+++ core/src/main/java/util/HTMLParser.java
@@ -0,0 +1,11 @@
+package util;
+
+/**
+ * this class is responsible for parsing html.
+ * it does things like:
+ * find list of links, list of resources;
+ *
+ * Created by Yannic on 18-5-17.
+ */
+public class HTMLParser {
+}
