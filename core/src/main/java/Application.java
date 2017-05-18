f6eea6bb93e6b67ec6e0310a1fe58e31eefe1aee
Yannic Riesebos <zebybez@surfspot.nu>
2017-05-18 21:47:18 +0200

    unreasonable commit, changed core main method

--- core/src/main/java/Application.java
+++ core/src/main/java/Application.java
@@ -2,6 +2,12 @@ import model.WebsiteEntity;
 import util.DBUtil;
 
 import javax.persistence.EntityManager;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by Marc on 3-5-2017.
@@ -9,7 +15,12 @@ import javax.persistence.EntityManager;
 public class Application {
 
     public static void main(String[] args) {
+        String startUrl = "http://stackoverflow.com/";
+        Crawler mainCrawler = new Crawler(startUrl, 2);
 
+    }
+
+    private static void testPersistance(){
         EntityManager entityManager = DBUtil.getEntityManager();
 
         WebsiteEntity w = new WebsiteEntity();
@@ -19,7 +30,6 @@ public class Application {
         entityManager.getTransaction().begin();
         entityManager.persist(w);
         entityManager.getTransaction().commit();
-
     }
 
 }
